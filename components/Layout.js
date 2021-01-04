f281f670c0bcd75e98d6568e81f207be9b4b77c2
sikeda107 <s.ikeda107@gmail.com>
2021-01-04 17:37:04 +0900

    add: Homeを作成 レイアウトを修正

--- components/Layout.js
+++ components/Layout.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 export default function Layout(props) {
   return (
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <header> Header </header>
+        <header>
+          <Typography
+            component="h2"
+            variant="h5"
+            color="inherit"
+            align="center"
+            noWrap
+          >
+            sikeda107 個人サイト
+          </Typography>
+        </header>
         <main>
           <Grid container spacing={5}>
-            <h1> Side </h1>
-            {props.children}
+            <Grid item xs={12} md={4}>
+              <h1>left</h1>
+            </Grid>
+            <Grid item xs={12} md={8}>
+              {props.children}
+            </Grid>
           </Grid>
         </main>
       </Container>
