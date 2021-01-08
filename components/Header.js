87b63743a2bdaeb0ed570e59f0e5b8bbd9ecb8e8
sikeda107 <s.ikeda107@gmail.com>
2021-01-08 23:03:00 +0900

    add: Header components

---
+++ components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography'
+
+export default function Header(props) {
+  return (
+    <header>
+      <Typography
+        component="h2"
+        variant="h5"
+        color="inherit"
+        align="center"
+        noWrap
+      >
+        sikeda107 個人サイト
+      </Typography>
+    </header>
+  )
+}
--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline'
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
+import Header from './Header'
 import Sidebar from './Sidebar'
 import Footer from './Footer'
 
@@ -11,17 +12,7 @@ export default function Layout(props) {
     <React.Fragment>
       <CssBaseline />
       <Container maxWidth="lg">
-        <header>
-          <Typography
-            component="h2"
-            variant="h5"
-            color="inherit"
-            align="center"
-            noWrap
-          >
-            sikeda107 個人サイト
-          </Typography>
-        </header>
+        <Header />
         <main>
           <Grid container spacing={5}>
             <Grid item xs={12} md={4}>
