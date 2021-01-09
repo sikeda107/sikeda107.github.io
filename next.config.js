93bea7b36242b1ba80232d7b18450c6422638710
sikeda107 <s.ikeda107@gmail.com>
2021-01-09 18:40:09 +0900

    add: imgixを追加

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  images: {
+    loader: 'imgix',
+    domains: ['sikeda107.imgix.net'],
+  },
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
